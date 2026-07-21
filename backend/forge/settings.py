1d538c0244143c032503dcded59492b17975975c
stahiomar <omarstahi2002@gmail.com>
2026-07-21 11:20:36 +0100

    Fully deployed the forge app in kubernetes and exposed it

--- backend/forge/settings.py
+++ backend/forge/settings.py
@@ -27,8 +27,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 # Application definition
 
