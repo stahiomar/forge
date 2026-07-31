660151837f614c725af0e315469de2a89f214cde
stahiomar <omarstahi2002@gmail.com>
2026-07-31 11:44:33 +0100

    Introduced our aws provider

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+    type = string
+}
+
+variable "aws_region" {}
+
+variable "environment" {}
+
+variable "vpc_name" {}
+
+variable "vpc_cidr" {}
+
+variable "public_subnet_cidr" {}
+
+variable "private_subnet_cidr" {}
