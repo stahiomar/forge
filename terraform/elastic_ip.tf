3391fb0cccfa39b411d9ab510a16aebe593bb057
stahiomar <omarstahi2002@gmail.com>
2026-08-02 14:46:26 +0100

    Added comment for aws eip

--- terraform/elastic_ip.tf
+++ terraform/elastic_ip.tf
@@ -1,3 +1,4 @@
+# This resource creates an Elastic IP address for the NAT Gateway. The EIP is associated with the NAT Gateway to allow instances in the private subnet to access the internet.
 resource "aws_eip" "nat" {
   tags = {
     Name        = "${var.vpc_name}-${var.environment}-nat-eip"
