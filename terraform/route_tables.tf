c533e02b520b6f6f3a5aeb8b923a68391091308c
stahiomar <omarstahi2002@gmail.com>
2026-08-02 15:01:37 +0100

    Added private route table

--- terraform/route_tables.tf
+++ terraform/route_tables.tf
@@ -21,3 +21,34 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+
+
+
+
+
+
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.vpc_name}-${var.environment}-private-rt"
+    Environment = var.environment
+    Project     = "Forge"
+  }
+}
+
+resource "aws_route" "nat" {
+  route_table_id = aws_route_table.private.id
+
+  destination_cidr_block = "0.0.0.0/0"
+
+  #Which gateway should traffic go to?
+  nat_gateway_id = aws_nat_gateway.nat_gateway.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
