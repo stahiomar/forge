361404d045395bd886a4e5423150ba0c87775f59
stahiomar <omarstahi2002@gmail.com>
2026-08-05 11:26:30 +0100

    Added security groups for postgres and redis

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,3 +1,4 @@
+# Security Groups
 resource "aws_security_group" "load_balancer" {
   vpc_id      = aws_vpc.main.id
   name        = "${var.vpc_name}-${var.environment}-load-balancer-sg"
@@ -20,6 +21,29 @@ resource "aws_security_group" "backend" {
   }
 }
 
+resource "aws_security_group" "postgres" {
+  vpc_id      = aws_vpc.main.id
+  name        = "${var.vpc_name}-${var.environment}-postgres-sg"
+  description = "Security group for the PostgreSQL database"
+  tags = {
+    Name        = "${var.vpc_name}-${var.environment}-postgres-sg"
+    Environment = var.environment
+    Project     = "Forge"
+  }
+}
+
+resource "aws_security_group" "redis" {
+  vpc_id      = aws_vpc.main.id
+  name        = "${var.vpc_name}-${var.environment}-redis-sg"
+  description = "Security group for the Redis cache"
+  tags = {
+    Name        = "${var.vpc_name}-${var.environment}-redis-sg"
+    Environment = var.environment
+    Project     = "Forge"
+  }
+}
+
+# Ingress rules
 resource "aws_vpc_security_group_ingress_rule" "https" {
 
   security_group_id = aws_security_group.load_balancer.id
@@ -47,4 +71,32 @@ resource "aws_vpc_security_group_ingress_rule" "backend_from_lb" {
   
   # That one means who initiates the connection, in this case the load balancer
   referenced_security_group_id = aws_security_group.load_balancer.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "redis_from_backend" {
+
+  security_group_id = aws_security_group.redis.id
+
+  from_port = 6379
+
+  to_port   = 6379
+
+  ip_protocol = "tcp"
+  
+  # That one means who initiates the connection, in this case the backend
+  referenced_security_group_id = aws_security_group.backend.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "postgres_from_backend" {
+
+  security_group_id = aws_security_group.postgres.id
+
+  from_port = 5432
+
+  to_port   = 5432
+
+  ip_protocol = "tcp"
+  
+  # That one means who initiates the connection, in this case the backend
+  referenced_security_group_id = aws_security_group.backend.id
 }
