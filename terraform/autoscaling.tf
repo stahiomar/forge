e4960144676a32976ff7f3ae32c23edd5755f384
stahiomar <omarstahi2002@gmail.com>
2026-08-13 14:24:19 +0100

    Added autoscaling group

---
+++ terraform/autoscaling.tf
@@ -0,0 +1,21 @@
+# Autoscaling group for backend instances to set how much instances we want to run and where
+# And what is the recipe to launch the instances with
+resource "aws_autoscaling_group" "backend" {
+
+  name = "${var.vpc_name}-${var.environment}-backend"
+
+  min_size = 2
+
+  max_size = 10
+
+  desired_capacity = 2
+
+  vpc_zone_identifier = [
+    aws_subnet.private.id
+  ]
+
+  launch_template {
+    id      = aws_launch_template.backend.id
+    version = "$Latest"
+  }
+}
